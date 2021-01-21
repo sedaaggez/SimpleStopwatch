cf3b899157ec18426be0b321f58ad0fb5fe744bc
Seda Aggez <sedaaggez56@gmail.com>
2021-01-21 20:56:33 +0300

    first commit

---
+++ app/src/main/java/com/sedaaggez/simplestopwatch/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.sedaaggez.simplestopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var number = 0
+    var runnable : Runnable = Runnable {  }
+    var handler : Handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    fun start(view: View) {
+        number = 0
+
+        runnable = object : Runnable {
+            override fun run() {
+                number = number + 1
+                textViewTime.text = "Time: $number"
+                handler.postDelayed(this,1000)
+            }
+        }
+        handler.post(runnable)
+    }
+
+    fun stop(view: View) {
+        handler.removeCallbacks(runnable)
+        number = 0
+        textViewTime.text = "Time: 0"
+    }
+}
